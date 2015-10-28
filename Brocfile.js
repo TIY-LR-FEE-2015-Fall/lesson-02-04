e3aa445d7a455add42cea24dea2045b47957316f
Ryan Tablada <ryan.tablada@gmail.com>
2015-10-28 11:42:01 -0500

    Use imports and style paths

--- Brocfile.js
+++ Brocfile.js
@@ -1,6 +1,12 @@
 var Merge = require('broccoli-merge-trees');
 var Sass = require('broccoli-sass');
 
-var styles = new Sass(['sass'], 'app.scss', 'app.css');
+var stylePaths = [
+  'sass',
+  'bower_components/normalize-css',
+  'bower_components/font-awesome/scss',
+];
 
-module.exports = new Merge(['public', styles, 'bower_components/font-awesome', 'bower_components/normalize-css'], {overwrite: true});
+var styles = new Sass(stylePaths, 'app.scss', 'app.css');
+
+module.exports = new Merge(['public', styles, 'bower_components/font-awesome/fonts'], {overwrite: true});
