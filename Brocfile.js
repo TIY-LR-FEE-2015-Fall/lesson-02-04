19ebb8ea69d089ecb1bbfffe244a61a75eb51b2e
Ryan Tablada <ryan.tablada@gmail.com>
2015-10-28 11:30:15 -0500

    Setup SASS

--- Brocfile.js
+++ Brocfile.js
@@ -1,3 +1,6 @@
 var Merge = require('broccoli-merge-trees');
+var Sass = require('broccoli-sass');
 
-module.exports = new Merge(['public', 'bower_components/font-awesome', 'bower_components/normalize-css'], {overwrite: true});
+var styles = new Sass(['sass'], 'app.scss', 'app.css');
+
+module.exports = new Merge(['public', styles, 'bower_components/font-awesome', 'bower_components/normalize-css'], {overwrite: true});
